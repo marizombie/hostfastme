50d819dc8293a4b2c8c9c3e10aa7ff776a547812
Maryna <marinklokova@gmail.com>
2025-01-15 15:25:45 +0100

    correcting umami component

--- components/UmamiScript.tsx
+++ components/UmamiScript.tsx
@@ -7,9 +7,9 @@ export default function UmamiScript(): null {
     if (!document.getElementById("umami-script")) {
       const script = document.createElement("script");
       script.id = "umami-script";
-      script.src = config.umamiScriptPath;
+      script.src = "/uma/script.js";
       script.defer = true;
-      script.setAttribute("data-website-id", config.umamiWebsiteId);
+      script.setAttribute("data-website-id", "640263a4-b3f8-4e99-8fd1-5c4ab656e712");
       document.head.appendChild(script);
       console.log("Umami script added");
     }
